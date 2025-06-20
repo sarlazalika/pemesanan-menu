488e0e399df58232fc3b1bad8c763f7e721816c4
sarlazalika <sarlazalika@gmail.com>
2025-06-20 15:57:33 +0700

    initialize Laravel project

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductTypeController;
-use App\Http\Controllers\CatalogController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\SalesController;
-use App\Http\Controllers\TableController;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/menu', [ProductController::class, 'index']);
-Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
-Route::patch('/cart/update/{product}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/remove/{product}', [CartController::class, 'remove'])->name('cart.remove');
-Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/checkout', [PaymentController::class, 'index'])->name('payment.index');
-Route::post('/checkout/process', [PaymentController::class, 'processPayment'])->name('payment.process');
-Route::get('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
-
-Route::resource('products', ProductController::class);
-Route::resource('product-types', ProductTypeController::class);
-Route::resource('orders', OrderController::class);
-
-Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
-
-Route::resource('tables', TableController::class);
-
-Route::resource('users', UserController::class);
+require __DIR__.'/auth.php';
